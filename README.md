ed2898fa51fb64377f5a4caf683f3f1900776b58
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2023-10-12 20:07:34 +0900

    add html files

--- README.md
+++ README.md
@@ -5,5 +5,8 @@ https://github.com/jigintern/intern-dev-tutorial/blob/main/deno/README.md
 これを参考に
 
 # Deno Deploy導入済み
-mainにpushすると自動的にdeployされてサイトが確認できます
-上のチェックマークから入れます
+任意のブランチにpushすると自動的にdeployされてサイトが確認できます
+https://github.com/Wholesome-Student/ToF/deploymentsから見れます
+
+# 現在の状況
+https://sweet-weasel-28.deno.dev
--- public/index.js
+++ public/index.js
@@ -1,13 +1,33 @@
-document.getElementById("greeting").onclick = async (e) => {
+document.getElementById("rules").onclick = async (e) => {
     e.preventDefault();
-    const msg = document.getElementById("msg").value;
-  
-    const response = await fetch("/hello",{
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        msg: msg
-      })
-    });
-    document.getElementById("title").innerText = await response.text();
+    document.location.assign('rules.html');
 }
+
+document.getElementById("settings").onclick = async (e) => {
+    e.preventDefault();
+    document.location.assign('settings.html');
+}
+
+document.getElementById("home").onclick = async (e) => {
+    e.preventDefault();
+    document.location.assign('index.html');
+}
+
+document.getElementById("map").onclick = async (e) => {
+    e.preventDefault();
+    document.location.assign('map.html');
+}
+
+// document.getElementById("rules").onclick = async (e) => {
+//   e.preventDefault();
+//   const msg = document.getElementById("msg").value;
+
+//   const response = await fetch("/hello",{
+//     method: 'POST',
+//     headers: {'Content-Type': 'application/json'},
+//     body: JSON.stringify({
+//       msg: msg
+//     })
+//   });
+//   document.getElementById("title").innerText = await response.text();
+// }
