9bc79b95f504676bc5e45d5b1afc506b9e4a8cf2
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2023-10-16 10:34:27 +0900

    fix compass 3

--- public/compass.js
+++ public/compass.js
@@ -47,9 +47,29 @@ function orientation(event) {
     }
 
     let direction;
-    direction = get8directions(degrees);
+    if (
+        (degrees > 337.5 && degrees < 360) ||
+        (degrees > 0 && degrees < 22.5)
+    ) {
+        direction = "北";
+    } else if (degrees > 22.5 && degrees < 67.5) {
+        direction = "北東";
+    } else if (degrees > 67.5 && degrees < 112.5) {
+        direction = "東";
+    } else if (degrees > 112.5 && degrees < 157.5) {
+        direction = "東南";
+    } else if (degrees > 157.5 && degrees < 202.5) {
+        direction = "南";
+    } else if (degrees > 202.5 && degrees < 247.5) {
+        direction = "南西";
+    } else if (degrees > 247.5 && degrees < 292.5) {
+        direction = "西";
+    } else if (degrees > 292.5 && degrees < 337.5) {
+        direction = "北西";
+    }
 
-    document.querySelector("#direction").innerHTML = direction + " : " + degrees;
+    document.querySelector("#direction").innerHTML =
+        direction + " : " + degrees;
     document.querySelector("#absolute").innerHTML = absolute;
     document.querySelector("#alpha").innerHTML = alpha;
     document.querySelector("#beta").innerHTML = beta;
