af2ea056c67e8dbbe7814d20c40b39269406ee8f
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2023-10-16 09:57:37 +0900

    add fullscreen mode

---
+++ public/index.js
@@ -0,0 +1,81 @@
+document.getElementById("home").onclick = async (e) => {
+    e.preventDefault();
+    document.location.assign('index.html');
+}
+
+document.getElementById("item").onclick = async (e) => {
+    e.preventDefault();
+    document.getElementById('menu-container').classList.toggle('open');
+}
+
+document.getElementById("map").onclick = async (e) => {
+    e.preventDefault();
+    document.location.assign('map.html');
+}
+
+document.getElementById("rules").onclick = async (e) => {
+    e.preventDefault();
+    document.location.assign('rules.html');
+}
+
+document.getElementById("fullscreen").onclick = async (e) => {
+    e.preventDefault();
+    if (document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement) {
+        if (document.cancelFullScreen) {
+        document.cancelFullScreen();
+        } else {
+        if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else {
+            if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+            }
+        }
+        }
+    } else {
+        const _element = document.documentElement;
+        if (_element.requestFullscreen) {
+        _element.requestFullscreen();
+        } else {
+        if (_element.mozRequestFullScreen) {
+            _element.mozRequestFullScreen();
+        } else {
+            if (_element.webkitRequestFullscreen) {
+            _element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+            }
+        }
+        }
+    }
+    };
+
+    const userAgent = window.navigator.userAgent;
+
+    const iPadSafari =
+    !!userAgent.match(/iPad/i) &&     // Detect iPad first.
+    !!userAgent.match(/WebKit/i) &&   // Filter browsers with webkit engine only
+    !userAgent.match(/CriOS/i) &&   // Eliminate Chrome & Brave
+    !userAgent.match(/OPiOS/i) &&   // Rule out Opera
+    !userAgent.match(/FxiOS/i) &&   // Rule out Firefox
+    !userAgent.match(/FocusiOS/i);    // Eliminate Firefox Focus as well!
+
+    const element = document.getElementById('fullScreenButton');
+
+    function iOS() {
+    if (userAgent.match(/ipad|iphone|ipod/i)) {
+        const iOS = {};
+        iOS.majorReleaseNumber = +userAgent.match(/OS (\d)?\d_\d(_\d)?/i)[0].split('_')[0].replace('OS ', '');
+        return iOS;
+    }
+    }
+
+    if (element !== null) {
+    if (userAgent.match(/iPhone/i) || userAgent.match(/iPod/i)) {
+        element.className += ' hidden';
+    } else if (userAgent.match(/iPad/i) && iOS().majorReleaseNumber < 12) {
+        element.className += ' hidden';
+    } else if (userAgent.match(/iPad/i) && !iPadSafari) {
+        element.className += ' hidden';
+    } else {
+        element.addEventListener('click', _toggleFullScreen, false);
+    }
+}
