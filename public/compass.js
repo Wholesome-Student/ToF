9a3733c1b050e77c889c262835c0d51aadbfb851
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2023-10-16 21:41:44 +0900

    fix compass for ios

--- public/compass.js
+++ public/compass.js
@@ -40,8 +40,7 @@ function orientation(event) {
 
     if(os == "iphone") {
         // webkitCompasssHeading値を採用
-        //degrees = event.webkitCompassHeading;
-        degrees = compassHeading(alpha, beta, gamma);
+        degrees = event.webkitCompassHeading;
     } else {
         // deviceorientationabsoluteイベントのalphaを補正
         degrees = compassHeading(alpha, beta, gamma);
