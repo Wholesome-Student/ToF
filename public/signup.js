6ddfa74795b9865dd227233fdc88b5a4ee1257f2
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2023-10-20 21:38:26 +0900

    add signup

---
+++ public/signup.js
@@ -0,0 +1,46 @@
+function checkRegex(s) {
+    const regex = /^[a-zA-Z0-9]+$/;
+  
+    // 正規表現に合致しない文字が存在するかをチェック
+    const unmatched = s.replace(regex, '');
+    
+    // 合致しない文字の有無を返す
+    return (unmatched.length == 0);
+}
+
+document.getElementById("signup").onclick = async(e) => {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "") {
+        document.getElementById("log").textContent = "ユーザー名を入力してください";
+    } else if (password === "") {
+        document.getElementById("log").textContent = "パスワードを入力してください";
+    } else if (!checkRegex(username)) {
+        document.getElementById("log").textContent = "ユーザー名には英数字のみ使用できます";
+    } else if (!checkRegex(password)) {
+        document.getElementById("log").textContent = "パスワードには英数字のみ使用できます";
+    } else {
+        try {
+            const res = await fetch("/signup", {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                })
+            });
+            console.log(res.status);
+            if (res.status === 200) {
+                localStorage.setItem("username", username);
+                window.location.assign("../");
+            } else {
+                document.getElementById("log").textContent = "サーバー側の不具合が発生しました";
+            }
+        } catch (error) {
+            document.getElementById("log").textContent = "クライアント側の不具合が発生しました";
+            console.log(error);
+        }
+    }
+}
--- server.deno.js
+++ server.deno.js
@@ -4,37 +4,46 @@ import "https://deno.land/std@0.193.0/dotenv/load.ts";
 import { Client } from "https://deno.land/x/mysql@v2.11.0/mod.ts";
 
 // MySQLのDBにアクセスして、操作用のクライアントを生成
-const mySqlClient = await new Client().connect({
+const sql = await new Client().connect({
     hostname: Deno.env.get("MYSQL_HOSTNAME"),
     username: Deno.env.get("MYSQL_USER"),
     password: Deno.env.get("MYSQL_PASSWORD"),
     db: Deno.env.get("MYSQL_DBNAME")
-})
+});
 
-// // SELECTなど、取得用SQLを実行する
-const selectResult = await mySqlClient.query(`SELECT * FROM teacher_futaba;`)
-console.log(selectResult)
-
-// // INSERTなど、書込用SQLを実行する
-// const insertResult = await mySqlClient.execute(`
-//     INSERT INTO teacher_futaba (
-//         name, joining_date
-//     ) VALUES (
-//         ?, ?
-//     );
-// `, [
-//     "じぐ美先生",
-//     new Date()
-//     ]
-// )
+async function signup(username, password) {
+    // ユーザー追加
+    await sql.execute(`
+        INSERT INTO login (
+            ??, ??
+        ) VALUES (
+            ?, ?
+        );
+    `, [
+        "username",
+        "password",
+        username,
+        password
+        ]
+    )
+}
 
 serve(async (req) => {
     const pathname = new URL(req.url).pathname;
 
-    if (req.method === "POST" && pathname === "/hello") {
-        const reqJson = await req.json();
-        let greet = "Hello, " + reqJson.msg + "!";
-        return new Response(greet);
+    if (req.method === "POST" && pathname === "/signup") {
+        try {
+            const reqJson = await req.json();
+            signup(reqJson.username, reqJson.password);
+            return new Response(null, {
+                status: 200,
+            });
+        } catch (error) {
+            console.error(error);
+            return new Response(null, {
+                status: 500,
+            });
+        }
     }
 
     return serveDir(req, {
