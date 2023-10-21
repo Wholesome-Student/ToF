2105f474937661870564b407d52a32fbb1d5bbc3
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2023-10-21 23:09:15 +0900

    fix needle

--- public/compass.js
+++ public/compass.js
@@ -4,14 +4,12 @@ let os;
 // DOM構築完了イベントハンドラ登録
 window.addEventListener("DOMContentLoaded", init);
 
-// 初期化
+/* 初期化処理 */
 function init() {
-    // 簡易的なOS判定
-    os = detectOSSimply();
-    if (os == "iphone") {
-        // safari用。DeviceOrientation APIの使用をユーザに許可して貰う
-        document.querySelector("#permit").addEventListener("click", permitDeviceOrientationForSafari);
+    os = detectOSSimply();  // os判定
 
+    if (os == "iphone") {
+        document.querySelector("#permit").addEventListener("click", permitDeviceOrientationForSafari);
         window.addEventListener(
             "deviceorientation",
             orientation,
@@ -24,51 +22,26 @@ function init() {
             true
         );
     } else {
-        window.alert("PC未対応サンプル");
+        window.alert("このデバイスには対応していません");
     }
 }
 
-
-// ジャイロスコープと地磁気をセンサーから取得
+/* 方角を取得 */
 function orientation(event) {
-    const absolute = event.absolute;
     const alpha = event.alpha;
-    const beta = event.beta;
-    const gamma = event.gamma;
     let degrees;
 
     if(os == "iphone") {
-        degrees = event.webkitCompassHeading;
+        degrees = 360 - event.webkitCompassHeading;
     } else {
-        degrees = 360 - alpha;
+        degrees = alpha;
     }
 
-    let direction;
-    if ((degrees > 337.5 && degrees < 360) || (degrees > 0 && degrees < 22.5)) {
-        direction = "北";
-    } else if (degrees > 22.5 && degrees < 67.5) {
-        direction = "北東";
-    } else if (degrees > 67.5 && degrees < 112.5) {
-        direction = "東";
-    } else if (degrees > 112.5 && degrees < 157.5) {
-        direction = "東南";
-    } else if (degrees > 157.5 && degrees < 202.5) {
-        direction = "南";
-    } else if (degrees > 202.5 && degrees < 247.5) {
-        direction = "南西";
-    } else if (degrees > 247.5 && degrees < 292.5) {
-        direction = "西";
-    } else if (degrees > 292.5 && degrees < 337.5) {
-        direction = "北西";
-    }
-
-    const rotate = 360 - degrees;
-
-    document.querySelector("#direction").innerHTML = direction + " : " + degrees;
-    document.getElementById('needle').style.transform = 'rotate(' + rotate + 'deg)';
+    // コンパスの針を回転
+    document.getElementById('needle').style.transform = 'rotate(' + degrees + 'deg)';
 }
 
-// 簡易OS判定
+/* OSを判定 */
 function detectOSSimply() {
     let ret;
     if (
@@ -101,6 +74,8 @@ function permitDeviceOrientationForSafari() {
         .catch(console.error);
 }
 
+/* ボトムナビゲーション */
+// [HOME]
 document.getElementById('home').onclick = async (e) => {
     e.preventDefault();
     document.location.assign('./home.html');
