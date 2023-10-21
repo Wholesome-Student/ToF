6dd2b87c4aa7274b86aac7667ce0b210c85c7ca4
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2023-10-21 22:30:11 +0900

    add new compass

--- public/compass.js
+++ public/compass.js
@@ -66,43 +66,10 @@ function orientation(event) {
         direction = "北西";
     }
 
-    let rotate = 360 - degrees;
+    const rotate = 360 - degrees;
 
     document.querySelector("#direction").innerHTML = direction + " : " + degrees;
-    document.getElementById('image').style.transform = 'rotate(' + rotate + 'deg)';
-}
-
-// 端末の傾き補正（Android用）
-// https://www.w3.org/TR/orientation-event/
-function compassHeading(alpha, beta, gamma) {
-    var degtorad = Math.PI / 180; // Degree-to-Radian conversion
-
-    var _x = beta ? beta * degtorad : 0; // beta value
-    var _y = gamma ? gamma * degtorad : 0; // gamma value
-    var _z = alpha ? alpha * degtorad : 0; // alpha value
-
-    var cX = Math.cos(_x);
-    var cY = Math.cos(_y);
-    var cZ = Math.cos(_z);
-    var sX = Math.sin(_x);
-    var sY = Math.sin(_y);
-    var sZ = Math.sin(_z);
-
-    // Calculate Vx and Vy components
-    var Vx = -cZ * sY - sZ * sX * cY;
-    var Vy = -sZ * sY + cZ * sX * cY;
-
-    // Calculate compass heading
-    var compassHeading = Math.atan(Vx / Vy);
-
-    // Convert compass heading to use whole unit circle
-    if (Vy < 0) {
-        compassHeading += Math.PI;
-    } else if (Vx < 0) {
-        compassHeading += 2 * Math.PI;
-    }
-
-    return compassHeading * (180 / Math.PI); // Compass Heading (in degrees)
+    document.getElementById('needle').style.transform = 'rotate(' + rotate + 'deg)';
 }
 
 // 簡易OS判定
@@ -136,4 +103,27 @@ function permitDeviceOrientationForSafari() {
             }
         })
         .catch(console.error);
+}
+
+document.getElementById('home').onclick = async (e) => {
+    e.preventDefault();
+    document.location.assign('./home.html');
+}
+
+// [ITEM]
+document.getElementById("item").onclick = async (e) => {
+    e.preventDefault();
+    document.getElementById('item-mordal').classList.toggle('open');
+    if (document.getElementById('other-mordal').classList.contains('open')) {
+        document.getElementById('other-mordal').classList.toggle('open');
+    }
+}
+
+// [OTHER]
+document.getElementById("other").onclick = async (e) => {
+    e.preventDefault();
+    document.getElementById('other-mordal').classList.toggle('open');
+    if (document.getElementById('item-mordal').classList.contains('open')) {
+        document.getElementById('item-mordal').classList.toggle('open');
+    }
 }
