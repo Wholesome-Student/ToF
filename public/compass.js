aa8bef7007173f96fea48482f86a8d4f1d575b92
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2023-10-22 01:38:11 +0900

    fix needle

--- public/compass.js
+++ public/compass.js
@@ -26,6 +26,37 @@ function init() {
     }
 }
 
+function compassHeading(alpha, beta, gamma) {
+    var degtorad = Math.PI / 180; // Degree-to-Radian conversion
+
+    var _x = beta ? beta * degtorad : 0; // beta value
+    var _y = gamma ? gamma * degtorad : 0; // gamma value
+    var _z = alpha ? alpha * degtorad : 0; // alpha value
+
+    var cX = Math.cos(_x);
+    var cY = Math.cos(_y);
+    var cZ = Math.cos(_z);
+    var sX = Math.sin(_x);
+    var sY = Math.sin(_y);
+    var sZ = Math.sin(_z);
+
+    // Calculate Vx and Vy components
+    var Vx = -cZ * sY - sZ * sX * cY;
+    var Vy = -sZ * sY + cZ * sX * cY;
+
+    // Calculate compass heading
+    var compassHeading = Math.atan(Vx / Vy);
+
+    // Convert compass heading to use whole unit circle
+    if (Vy < 0) {
+        compassHeading += Math.PI;
+    } else if (Vx < 0) {
+        compassHeading += 2 * Math.PI;
+    }
+
+    return compassHeading * (180 / Math.PI); // Compass Heading (in degrees)
+}
+
 /* 端末の方角を取得 */
 function orientation(event) {
     const alpha = event.alpha;
@@ -34,7 +65,8 @@ function orientation(event) {
     if(os == "iphone") {
         degrees = 360 - event.webkitCompassHeading;
     } else {
-        degrees = alpha;
+        degrees = compassHeading(alpha, beta, gamma);
+        // degrees = alpha;
     }
 
 
