9e658fed130d11879d94a7e99eb6941e24e7cc42
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2023-10-23 08:56:10 +0900

    add map

--- public/signin.js
+++ public/signin.js
@@ -34,29 +34,47 @@ document.getElementById("signin").onclick = async (e) => {
             })
             if(res.status === 200) {
                 localStorage.setItem("username", username)
-                if (!localStorage.hasOwnProperty("score")) {
-                    localStorage.setItem("score", 0);
-                }
-                if (!localStorage.hasOwnProperty("number")) {
-                    localStorage.setItem("number", 0);
-                }
-                if (!localStorage.hasOwnProperty("check_qr")) {
-                    localStorage.setItem("check_qr", 0);
-                }
-                if (!localStorage.hasOwnProperty("check_quiz")) {
-                    localStorage.setItem("check_quiz", 0);
-                }
-                if (!localStorage.hasOwnProperty("check_now")) {
-                    localStorage.setItem("check_now", -1);
-                }
-                if (!localStorage.hasOwnProperty("random")) {
-                    const r = Math.floor(Math.random() * (6 + 1));
-                    const l = [1234, 1324, 2134, 2314, 3124, 3214];
-                    localStorage.setItem("random", l[r]);
-                }
-                if (!localStorage.hasOwnProperty("location")) {
-                    localStorage.setItem("location", 0);
-                }
+                // デバッグ
+                // 現在のスコア
+                localStorage.setItem("score", 0);
+                // 現在の問題番号
+                localStorage.setItem("number", 0);
+                // QRポイントをすでにスキャンしたか
+                localStorage.setItem("check_qr", 0);
+                // クイズはすでに終了したか
+                localStorage.setItem("check_quiz", 0);
+                // 今受けているクイズ
+                localStorage.setItem("check_now", -1);
+                
+                // 回る順序
+                const r = Math.floor(Math.random() * (6 + 1));
+                const l = [1234, 1324, 2134, 2314, 3124, 3214];
+                localStorage.setItem("random", l[r]);
+                // 回った場所の数
+                localStorage.setItem("location", 0);
+                // if (!localStorage.hasOwnProperty("score")) {
+                //     localStorage.setItem("score", 0);
+                // }
+                // if (!localStorage.hasOwnProperty("number")) {
+                //     localStorage.setItem("number", 0);
+                // }
+                // if (!localStorage.hasOwnProperty("check_qr")) {
+                //     localStorage.setItem("check_qr", 0);
+                // }
+                // if (!localStorage.hasOwnProperty("check_quiz")) {
+                //     localStorage.setItem("check_quiz", 0);
+                // }
+                // if (!localStorage.hasOwnProperty("check_now")) {
+                //     localStorage.setItem("check_now", -1);
+                // }
+                // if (!localStorage.hasOwnProperty("random")) {
+                //     const r = Math.floor(Math.random() * (6 + 1));
+                //     const l = [1234, 1324, 2134, 2314, 3124, 3214];
+                //     localStorage.setItem("random", l[r]);
+                // }
+                // if (!localStorage.hasOwnProperty("location")) {
+                //     localStorage.setItem("location", 0);
+                // }
                 window.location.assign("./home.html")
             } else if(res.status === 401) {
                 document.getElementById("log").textContent = "サインインに失敗しました"
