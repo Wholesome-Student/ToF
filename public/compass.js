b3fba6f1d869bcc9113605312829d65af354430a
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2023-10-22 20:09:03 +0900

    fix compass

--- public/compass.js
+++ public/compass.js
@@ -14,7 +14,7 @@ function adjust() {
 
 function reset() {    
     mode = 0;
-    document.getElementById("tutorial").innerText = "補正";
+    document.getElementById("tutorial").innerText = "補正完了";
 }
 
 /* 初期化処理 */
@@ -96,15 +96,16 @@ function orientation(event) {
         // 現在位置を取得
         const lat1 = position.coords.latitude;
         const lon1 = position.coords.longitude;
-        console.log("現在地:\n", Math.round(lat1 * 1000) / 1000, Math.round(lon1 * 1000) / 1000);
-        console.log("https://www.google.co.jp/maps/place/"+String(lat1)+"N+"+String(lon1)+"E")
+        // console.log("現在地:\n", Math.round(lat1 * 1000) / 1000, Math.round(lon1 * 1000) / 1000);
+        // console.log("https://www.google.co.jp/maps/place/"+String(lat1)+"N+"+String(lon1)+"E")
 
         // 目的地を取得
         // 高専
-        const lat2 = 35.1035074091722;
-        const lon2 = 137.1489685066174;
-        console.log("目的地:\n", Math.round(lat2 * 1000) / 1000, Math.round(lon2 * 1000) / 1000);
-        console.log("https://www.google.co.jp/maps/place/"+String(lat2)+"N+"+String(lon2)+"E")
+        
+        const lat2 = 35.078201201661;
+        const lon2 = 137.14594027778583;
+        // console.log("目的地:\n", Math.round(lat2 * 1000) / 1000, Math.round(lon2 * 1000) / 1000);
+        // console.log("https://www.google.co.jp/maps/place/"+String(lat2)+"N+"+String(lon2)+"E")
 
         // 背景を変更
         const distance = strength(lat1, lon1, lat2, lon2);
@@ -114,8 +115,8 @@ function orientation(event) {
         let needle;
 
         // コンパスの針を回転
-        // needle = degrees - dirG(lat1, lon1, lat2, lon2);
-        needle = degrees;
+        needle = degrees - dirG(lat1, lon1, lat2, lon2);
+        // needle = degrees;
         document.getElementById('needle').style.transform = 'rotate(' + needle + 'deg)';
     })
 }
