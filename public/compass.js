049e281b55a6da81a1d8bc7cfb1bcbb9a0f8d783
Wholesome-Student <E3ee4vxnk0a@outlook.jp>
2023-10-23 10:09:09 +0900

    fix all

--- public/compass.js
+++ public/compass.js
@@ -151,7 +151,7 @@ function dirG(lat1, lon1, lat2, lon2) {
     const x2 = lon2 * degToRad;
     const y2 = lat2 * degToRad;
 
-    const rotate = - Math.atan(
+    const rotate = 90 - Math.atan(
         (Math.sin(x2 - x1) / 
             (Math.cos(y1) * Math.tan(y2) - Math.sin(y1) * Math.cos(x2 - x1))
         )) * radToDeg;
