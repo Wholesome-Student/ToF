5e84aa116330d5a3a605ce0b6c834588c02c3658
32229Tsuge-EE <131430049+TsugeYuki-EE@users.noreply.github.com>
2023-12-14 22:10:15 +0900

    Update home.js

--- public/home.js
+++ public/home.js
@@ -2,7 +2,7 @@
 let pressStartTime;
 
 /* タイマー */
-const deadline = new Date('2023-10-23T16:10:00');
+const deadline = new Date('2023-12-24T00:00:00');
 const countdownInterval = setInterval(countdown, 1000);
 
 function countdown() {
